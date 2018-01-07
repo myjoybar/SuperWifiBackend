1bc14dc024f2d328a553978bffe5de9b1be45e0e
myjoybar <myjoybar@gmail.com>
2018-01-07 12:40:18 +0800

    first  commit

---
+++ src/main/java/com/joybar/superwifi/SuperWifiApplication.java
@@ -0,0 +1,12 @@
+package com.joybar.superwifi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperWifiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperWifiApplication.class, args);
+	}
+}
